cc58d9fa8da2ee7d5d60aa0789e59ffb3b14ddc5
andrei sili <andrei.sili1994@gmail.com>
2020-03-24 16:27:57 +0200

    Implemented GSNN model, runner, evaluator, and visualiser. Made optical flow reconstruction better. Changed specs. Various improvements.

--- models/i3d/vae/gsnn_i3d.py
+++ models/i3d/vae/gsnn_i3d.py
@@ -4,15 +4,14 @@ import torch as th
 from torch import nn
 
 from models.i3d.vae._classifier import I3DClassifier
-from models.i3d.vae._decoder import I3DDecoder
 from models.i3d.vae._encoder import I3DEncoder
 
-VAE_FORWARD = tp.Tuple[th.Tensor, th.Tensor, th.Tensor, th.Tensor, th.Tensor, th.Tensor, tp.Optional[th.Tensor]]
+GSNN_FORWARD = tp.Tuple[th.Tensor, th.Tensor, th.Tensor, th.Tensor, th.Tensor, tp.Optional[th.Tensor]]
 
 
-class VAEI3D(nn.Module):
+class GSNNI3D(nn.Module):
     def __init__(self, time_steps: int, latent_planes: int, dropout_prob: float, num_classes: int, vote_type: str):
-        super(VAEI3D, self).__init__()
+        super(GSNNI3D, self).__init__()
         assert vote_type in ['soft', 'hard'], f'Unknown vote type: {vote_type}.'
 
         self.time_steps = time_steps
@@ -22,7 +21,6 @@ class VAEI3D(nn.Module):
         self.vote_type = vote_type
 
         self.encoder = I3DEncoder(self.latent_planes)
-        self.decoder = I3DDecoder(self.latent_planes, self.time_steps)
         self.classifier = I3DClassifier(self.latent_planes, self.dropout_prob, self.num_classes)
 
         self.softmax = nn.Softmax(dim=-1)
@@ -39,23 +37,21 @@ class VAEI3D(nn.Module):
                 module.weight.data.fill_(1)
                 module.bias.data.zero_()
 
-    def forward(self, _in: th.Tensor, num_samples: int) -> VAE_FORWARD:
+    def forward(self, _in: th.Tensor, num_samples: int) -> GSNN_FORWARD:
         if self.training:
             return self._forward(_in, num_samples)
         else:
             return self._inference(_in, num_samples)
 
-    def _forward(self, _in: th.Tensor, num_samples: int) -> VAE_FORWARD:
+    def _forward(self, _in: th.Tensor, num_samples: int) -> GSNN_FORWARD:
 
         temporal_latents, _mean, _var = self.encoder(_in, num_samples)
-        _recon = self.decoder(temporal_latents)
         _pred, _class_latent = self.classifier(temporal_latents)
 
-        return _recon, _pred, temporal_latents, _class_latent, _mean, _var, None
+        return _pred, temporal_latents, _class_latent, _mean, _var, None
 
-    def _inference(self, _in: th.Tensor, num_samples: int) -> VAE_FORWARD:
+    def _inference(self, _in: th.Tensor, num_samples: int) -> GSNN_FORWARD:
         _temporal_latents, _mean, _var = self.encoder(_in, num_samples)
-        _recon = self.decoder(_temporal_latents)
         _pred, _class_latent = self.classifier(_temporal_latents)
 
         if self.vote_type == 'hard':
@@ -63,7 +59,7 @@ class VAEI3D(nn.Module):
         else:
             _vote = self._soft_vote(_pred)
 
-        return _recon, _pred, _temporal_latents, _class_latent, _mean, _var, _vote
+        return _pred, _temporal_latents, _class_latent, _mean, _var, _vote
 
     def _hard_vote(self, _preds: th.Tensor) -> th.Tensor:
         bs, num_samples, _ = _preds.shape
